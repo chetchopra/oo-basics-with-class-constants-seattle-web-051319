6f9b63767aaa9a8987644843d462ed24b3ea6394
Chet  Chopra <chopra.chet@gmail.com>
2019-05-15 20:19:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if !BRANDS.includes?()
-    BRANDS << brand
+    if !BRANDS.includes(brand)
+      BRANDS << brand
+    end
   end
   
 
